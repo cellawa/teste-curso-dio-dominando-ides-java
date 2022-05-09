803520acb564255a802bd51ae2a032dbaa80df7b
louko <w.riosmagalhaes@gmail.com>
2022-05-09 11:37:31 -0300

    First commit

---
+++ src/Dio/com/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package Dio.com;
+
+import Dio.com.modell.Gato;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		
+Gato gato = new Gato ();		
+Livros livros = new Livros();
+System.out.println(gato);
+System.out.println(livros);
+
+		/*System.out.println("hellow word!");*/
+	}
+
+	}
+class Livros {
+	private String nome;
+	private String npag;
+}
